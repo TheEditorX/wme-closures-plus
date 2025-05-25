a9a0a6d239901a02af9a259d5c6bc97037e23227
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-05-25 23:36:47 +0300

    refactor: avoid shadowing global "constructor" prop in `isInstance`

--- src/utils/is-instance.ts
+++ src/utils/is-instance.ts
@@ -7,9 +7,9 @@ export function isInstance(
 export function isInstance(
   value: unknown,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  constructor?: new (...args: any[]) => unknown,
+  ctor?: new (...args: any[]) => unknown,
 ): boolean {
-  if (constructor) return value instanceof constructor;
+  if (ctor) return value instanceof ctor;
 
   return (
     typeof value === 'object' &&
