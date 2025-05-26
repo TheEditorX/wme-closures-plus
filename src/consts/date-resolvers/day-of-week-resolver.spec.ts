fd73ad59195088ee404312c018b4a7057e5afece
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-05-26 19:36:02 +0300

    test(date-resolver): WeekdayFlags and remove obsolete test in DayOfWeek

--- src/consts/date-resolvers/day-of-week-resolver.spec.ts
+++ src/consts/date-resolvers/day-of-week-resolver.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from '@jest/globals';
 import '../../../tests/__extenders__/toBeDateEqual';
 import { DAY_OF_WEEK_RESOLVER } from './day-of-week-resolver';
-import { Weekday } from '../../enums';
+import { WeekdayFlags } from '../../enums';
 
 describe('DAY_OF_WEEK_RESOLVER', () => {
   beforeEach(() => {
@@ -15,7 +15,7 @@ describe('DAY_OF_WEEK_RESOLVER', () => {
 
   it('should resolve the next occurrence of the specified day of the week', () => {
     const result = DAY_OF_WEEK_RESOLVER.resolve({
-      dayOfWeek: Weekday.Tuesday,
+      dayOfWeek: WeekdayFlags.Tuesday.valueOf(),
     });
 
     expect(result.toDate()).toBeDateEqual(new Date('2025-04-22T13:00:00Z')); // Next Tuesday from the fixed date
@@ -24,20 +24,10 @@ describe('DAY_OF_WEEK_RESOLVER', () => {
 
   it("should return next week today's if the specified day of the week is today", () => {
     const result = DAY_OF_WEEK_RESOLVER.resolve({
-      dayOfWeek: Weekday.Thursday,
+      dayOfWeek: WeekdayFlags.Thursday.valueOf(),
     });
 
     expect(result.toDate()).toBeDateEqual(new Date('2025-04-24T13:00:00Z')); // Next Thursday from the fixed date
     expect(result.toDate()).toBeDateEqual(new Date('2025-04-24T05:00:00Z')); // Next Thursday from the fixed date without time comparison
-  });
-
-  it('should throw an error if the arguments are invalid', () => {
-    expect(() =>
-      DAY_OF_WEEK_RESOLVER.resolve({ dayOfWeek: -1 as Weekday }),
-    ).toThrow('Invalid arguments for DAY_OF_WEEK_RESOLVER: dayOfWeek=-1');
-
-    expect(() =>
-      DAY_OF_WEEK_RESOLVER.resolve({ dayOfWeek: null as unknown as Weekday }),
-    ).toThrow('Invalid arguments for DAY_OF_WEEK_RESOLVER: dayOfWeek=null');
   });
 });
