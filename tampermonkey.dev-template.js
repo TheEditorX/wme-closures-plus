75f804f428ea7562427d70bcf8decb85ad194d31
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-05-31 18:33:56 +0300

    fix: grant XHR access in the default meta.js file

--- tampermonkey.dev-template.js
+++ tampermonkey.dev-template.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name WME Closures+ [DEV]
 // @description WME Closures+ extends the Waze Map Editor with advanced closure management features
-// @version 1.0.0-beta.1
+// @version 1.0.0-beta.3
 // @author r0den
 // @match https://*.waze.com/*editor*
 // @match https://waze.com/*editor*
--- tampermonkey.meta.js
+++ tampermonkey.meta.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name WME Closures+
 // @description WME Closures+ extends the Waze Map Editor with advanced closure management features
-// @version 1.0.0-beta.1
+// @version 1.0.0-beta.3
 // @author r0den
 // @match https://*.waze.com/*editor*
 // @match https://waze.com/*editor*
 // @match https://*.wazestg.com/*editor*
 // @connect distributions.crowdin.net
 // @require https://cdn.jsdelivr.net/gh/WazeSpace/wme-sdk-plus@v1/wme-sdk-plus.js
+// @grant GM_xmlhttpRequest
 // ==/UserScript==
