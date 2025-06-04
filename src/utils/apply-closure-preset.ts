e02672ee41021b3fd716766e4e3722c98b0c0584
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-06-05 00:17:38 +0300

    feat: add debug logging to closure preset application process

--- src/utils/apply-closure-preset.ts
+++ src/utils/apply-closure-preset.ts
@@ -2,29 +2,60 @@ import { ClosureEditorForm, DateOnly } from 'classes';
 import { TimeOnly } from 'classes';
 import { getDateResolverByName } from 'consts/date-resolvers';
 import { ClosurePreset } from 'interfaces/closure-preset';
+import Logger from 'js-logger';
+
+const logger = Logger.get('closure-presets');
 
 function getStartDateForPreset(
   closureDetails: ClosurePreset['closureDetails'],
   defaultStartDate: DateOnly = new DateOnly(),
 ): Date {
-  const date =
-    closureDetails.startDate ?
-      getDateResolverByName(closureDetails.startDate.type).resolve(
-        closureDetails.startDate.args,
-      )
-    : defaultStartDate;
+  logger.debug('Getting start timestamp for preset');
 
-  const time =
-    closureDetails.startTime ?
-      new TimeOnly(
-        closureDetails.startTime.hours,
-        closureDetails.startTime.minutes,
-        0,
-        0,
-      )
-    : new TimeOnly();
+  const date = ((): DateOnly => {
+    if (!closureDetails.startDate) {
+      logger.debug('No specific start date supplied. Using default', {
+        default: defaultStartDate.toISOString(),
+      });
+      return defaultStartDate;
+    }
 
-  return date.withTime(time);
+    logger.debug('Using date resolver to get start date', {
+      resolver: closureDetails.startDate.type,
+    });
+    const result = getDateResolverByName(closureDetails.startDate.type).resolve(
+      closureDetails.startDate.args,
+    );
+    logger.debug('Resolved start date', {
+      resolver: closureDetails.startDate.type,
+      result: result.toISOString(),
+    });
+    return result;
+  })();
+
+  const time = ((): TimeOnly => {
+    if (!closureDetails.startTime) {
+      logger.debug('No specific start time supplied. Using current time');
+      return new TimeOnly();
+    }
+
+    logger.debug('Using specified time as start time', {
+      hours: closureDetails.startTime.hours,
+      minutes: closureDetails.startTime.minutes,
+    });
+    return new TimeOnly(
+      closureDetails.startTime.hours,
+      closureDetails.startTime.minutes,
+      0,
+      0,
+    );
+  })();
+
+  const result = date.withTime(time);
+  logger.debug('Resolved starting timestamp', {
+    value: result.toISOString(),
+  });
+  return result;
 }
 
 function getEndDateForPreset(
@@ -32,6 +63,10 @@ function getEndDateForPreset(
   startDate: Date,
 ): Date {
   const { end: endDetails } = closureDetails;
+
+  logger.debug('Getting end timestamp for preset', {
+    value: endDetails,
+  });
 
   switch (endDetails.type) {
     case 'DURATIONAL':
@@ -57,6 +92,13 @@ function getEndDateForPreset(
         endTime.getMilliseconds(),
       );
       if (endTime < startTime) {
+        logger.debug(
+          'Using fixed end time. End time is earlier than the start time, probably referring to the next day. Advances the date',
+          {
+            startTime: startTime.toString(),
+            endTime: endTime.toString(),
+          },
+        );
         // we need to apply the end time to the next day
         endDate.setDate(endDate.getDate() + 1);
       }
@@ -70,18 +112,31 @@ function getEndDateForPreset(
 }
 
 export function applyClosurePreset(
-  { closureDetails }: ClosurePreset,
+  { closureDetails, ...restPreset }: ClosurePreset,
   closureEditorForm: ClosureEditorForm,
 ) {
+  logger.debug('Beginning to apply a closure preset', {
+    presetMeta: restPreset,
+    closureDetails,
+  });
+
   const startDate = getStartDateForPreset(
     closureDetails,
     new DateOnly(closureEditorForm.getStart()),
   );
   const endDate = getEndDateForPreset(closureDetails, startDate);
 
-  if (closureDetails.description)
+  if (closureDetails.description) {
+    logger.log('Preset has a description. Applying..');
     closureEditorForm.setDescription(closureDetails.description);
+  }
 
+  logger.log('Applying resolved start timestamp', {
+    value: startDate.toISOString(),
+  });
   closureEditorForm.setStart(startDate);
+  logger.log('Applying resolved end timestamp', {
+    value: endDate.toISOString(),
+  });
   closureEditorForm.setEnd(endDate);
 }
